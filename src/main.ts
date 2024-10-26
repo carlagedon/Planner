f8e11eaa5833b9307f73cedbfac1823dd8e0e757
carlagedon <mukanelnru0@gmail.com>
2024-10-26 14:25:46 +0600

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+		exposedHeaders: 'set-cookie'
+  })
+
+  const config = new DocumentBuilder()
+    .setTitle('Planner')
+    .setDescription('Some Planer')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(3000);
+}
+
+bootstrap();
